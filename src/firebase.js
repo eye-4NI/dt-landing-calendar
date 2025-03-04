b5923f109a4337c97a761176a1c79b41e06d80d9
Eladio Limones <100542413+eye-4NI@users.noreply.github.com>
2025-03-03 23:05:30 -0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,8 +3,8 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  apiKey: process.env.,
+  authDomain: process.env.,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_SENDER_ID,
